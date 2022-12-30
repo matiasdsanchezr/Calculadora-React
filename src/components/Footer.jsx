4c6fbc82afadc8ada7df4952f45e9b9f67e92b56
Matias D. Sanchez <matiasdsanchezr@gmail.com>
2022-12-30 17:24:51 -0300

    Funcionalidad agregada

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import styles from './Footer.module.css';
 const Footer = () => {
   return (
     <footer className={styles.footerContainer}>
-      <h3>Pie de pagina</h3>
+      <h3>Codo a codo - 2022</h3>
     </footer>
   );
 };
--- src/pages/Home/Calculator.jsx
+++ src/pages/Home/Calculator.jsx
@@ -1,26 +1,88 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import styles from './Calculator.module.css';
+import { create, all, nthRoot } from 'mathjs';
 
 const Calculator = () => {
-  const [operation, setOperation] = useState("");
+  const [operation, setOperation] = useState('');
+  const [result, setResult] = useState('');
+  const [isShowingResult, setIsShowingResult] = useState(false);
 
-  const onButtonClickHandle = (value) => {
-    setOperation((curValue) => {
-      return curValue + value;
+  const operationRef = useRef();
+
+  const math = create(all);
+  math.import(
+    {
+      log: math.log10,
+      ln: math.log,
+      nthRoot: (root, number) => nthRoot(number, root),
+    },
+    { override: true }
+  );
+
+  const onClickButton = (value) => {
+    operationRef.current.focus();
+    checkIfShowingResult();
+    setOperation((operation) => operation.concat(value));
+  };
+
+  const onClickEqual = () => {
+    if (!operation) return;
+
+    const op = operation.replace(/√|∛/g, function (match) {
+      if (match === '√') return 'sqrt';
+      if (match === '∛') return 'cbrt';
+    });
+
+    try {
+      let newResult = math.evaluate(op);
+      newResult = math.format(newResult, {
+        notation: 'auto',
+        precision: 6,
+        lower: -4,
+        upper: 4,
+      });
+      setResult(newResult);
+    } catch (error) {
+      setResult('Syntax Error');
+    } finally {
+      setIsShowingResult(true);
+    }
+  };
+
+  // TODO Agregar simbolo para raiz enésima
+  const onClickNthRoot = () => {
+    const lastNumber = operation.match(/\d+$/);
+    if (!lastNumber) return;
+    setOperation((operation) => {
+      return operation
+        .slice(0, -lastNumber[0].length)
+        .concat(`nthRoot(${lastNumber}, `);
     });
   };
 
-  const Button = ({ text, value, alt = false }) => {
-    return (
-      <button
-        className={`${styles.calculatorButton} ${alt ? styles.bg2 : null}`}
-        value={value}
-        onClick={() => onButtonClickHandle(value)}
-      >
-        {text}
-      </button>
-    );
+  const onClickMemoryRead = () => {
+    checkIfShowingResult();
+    if (!result || result === 'Syntax error') return;
+    setOperation((operation) => operation.concat(result));
   };
+
+  const checkIfShowingResult = () => {
+    if (isShowingResult) {
+      setIsShowingResult(false);
+      setOperation('');
+    }
+  };
+
+  const allClear = () => {
+    setOperation('');
+    setResult('');
+  };
+
+  function moveCaretAtEnd(e) {
+    var temp_value = e.target.value;
+    e.target.value = '';
+    e.target.value = temp_value;
+  }
 
   return (
     <div className={styles.container}>
@@ -31,58 +93,79 @@ const Calculator = () => {
               className={styles.calculatorOperation}
               placeholder={operation ?? 'Ingresa una operacion...'}
               readOnly={true}
+              ref={operationRef}
+              onFocus={moveCaretAtEnd}
             />
             <input
               id="result"
               className={styles.calculatorResult}
               placeholder="="
               readOnly={true}
+              value={'= ' + result}
             />
           </div>
         </div>
         <div className={styles.calculatorKeyboard}>
-          <Button text="(" value="(" alt={true}></Button>
-          <Button text=")" value=")" alt={true}></Button>
-          <Button text="x!" value="x!" alt={true}></Button>
-          <Button text="x²" value="x²" alt={true}></Button>
-          <Button text="√" value="√" alt={true}></Button>
-          <Button text="ⁿ√" value="ⁿ√" alt={true}></Button>
-          <Button text="sin" value="sin" alt={true}></Button>
-          <Button text="cos" value="cos" alt={true}></Button>
-          <Button text="tg" value="tg" alt={true}></Button>
-          <Button text="x³" value="x³" alt={true}></Button>
-          <Button text="∛" value="∛" alt={true}></Button>
-          <Button text="xⁿ" value="xⁿ" alt={true}></Button>
-          <Button text="7" value="7"></Button>
-          <Button text="8" value="8"></Button>
-          <Button text="9" value="9"></Button>
-          <Button text="/" value="/" alt={true}></Button>
-          <Button text="%" value="%" alt={true}></Button>
-          <Button text="ln" value="ln" alt={true}></Button>
-          <Button text="4" value="4"></Button>
-          <Button text="5" value="5"></Button>
-          <Button text="6" value="6"></Button>
-          <Button text="*" value="*" alt={true}></Button>
-          <Button text="log" value="log" alt={true}></Button>
-          <Button text="e" value="e" alt={true}></Button>
-          <Button text="1" value="1"></Button>
-          <Button text="2" value="2"></Button>
-          <Button text="3" value="3"></Button>
-          <Button text="-" value="-" alt={true}></Button>
-          <Button text="AC" value="AC" alt={true}></Button>
-          <Button text="." value="." alt={true}></Button>
-          <Button text="0" value="0"></Button>
-          <Button text="π" value="π" alt={true}></Button>
-          <Button text="+" value="+" alt={true}></Button>
-          <Button text="MR" value="MR" alt={true}></Button>
+          <Button text="(" value="(" alt={true} onClick={onClickButton} />
+          <Button text=")" value=")" alt={true} onClick={onClickButton} />
+          <Button text="x!" value="!" alt={true} onClick={onClickButton} />
+          <Button text="x²" value="^2" alt={true} onClick={onClickButton} />
+          <Button text="√" value="√(" alt={true} onClick={onClickButton} />
+          <Button text="ⁿ√" value="ⁿ√" alt={true} onClick={onClickNthRoot} />
+          <Button text="sin" value="sin(" alt={true} onClick={onClickButton} />
+          <Button text="cos" value="cos(" alt={true} onClick={onClickButton} />
+          <Button text="tg" value="tg(" alt={true} onClick={onClickButton} />
+          <Button text="x³" value="^3" alt={true} onClick={onClickButton} />
+          <Button text="∛" value="∛(" alt={true} onClick={onClickButton} />
+          <Button text="xⁿ" value="^" alt={true} onClick={onClickButton} />
+          <Button text="7" value="7" onClick={onClickButton} />
+          <Button text="8" value="8" onClick={onClickButton} />
+          <Button text="9" value="9" onClick={onClickButton} />
+          <Button text="/" value="/" alt={true} onClick={onClickButton} />
+          <Button text="%" value="%" alt={true} onClick={onClickButton} />
+          <Button text="ln" value="ln(" alt={true} onClick={onClickButton} />
+          <Button text="4" value="4" onClick={onClickButton} />
+          <Button text="5" value="5" onClick={onClickButton} />
+          <Button text="6" value="6" onClick={onClickButton} />
+          <Button text="*" value="*" alt={true} onClick={onClickButton} />
+          <Button text="log" value="log(" alt={true} onClick={onClickButton} />
+          <Button text="e" value="e" alt={true} onClick={onClickButton} />
+          <Button text="1" value="1" onClick={onClickButton} />
+          <Button text="2" value="2" onClick={onClickButton} />
+          <Button text="3" value="3" onClick={onClickButton} />
+          <Button text="-" value="-" alt={true} onClick={onClickButton} />
+          <button
+            className={`${styles.calculatorButton} ${styles.bg2}`}
+            onClick={allClear}
+          >
+            AC
+          </button>
+          <Button text="." value="." alt={true} onClick={onClickButton} />
+          <Button text="0" value="0" onClick={onClickButton} />
+          <Button text="π" value="pi" alt={true} onClick={onClickButton} />
+          <Button text="+" value="+" alt={true} onClick={onClickButton} />
+          <Button text="MR" alt={true} onClick={onClickMemoryRead} />
           <button
             className={`${styles.calculatorButton} ${styles.calculatorButtonEqual}`}
+            onClick={onClickEqual}
           >
             =
           </button>
         </div>
       </div>
     </div>
+  );
+};
+
+const Button = ({ text, value, alt = false, onClick }) => {
+  return (
+    <button
+      className={`${styles.calculatorButton} ${alt ? styles.bg2 : null}`}
+      value={value}
+      onClick={() => onClick(value)}
+    >
+      {text}
+    </button>
   );
 };
 
